84ad92d49b1d1167adc515f9fff7ecfeb3674d7f
kakarlavinodkumar <kakarlavinodkumar@missionrnd.com>
2016-03-31 12:48:23 -0700

    Submit Commit

--- src/BSTClosestLeafDistance.cpp
+++ src/BSTClosestLeafDistance.cpp
@@ -39,7 +39,82 @@ struct node{
   struct node *right;
 };
 
+struct node *search(struct node *root, int key, int *distance)
+{
+	struct node *temp;
+	for (temp = root; temp != NULL;)
+	{
+		if (temp->data == key)
+			return temp;
+		else
+		{
+			(*distance)++;
+			if (key > temp->data)
+				temp = temp->right;
+			else
+				temp = temp->left;
+		}
+	}
+	return NULL;
+}
+int minimum_height_of_subtree(struct node *temp1)
+{
+	if (temp1->left == NULL&&temp1->right == NULL)
+		return 0;
+	else if (temp1->left == NULL)
+		return 1 + minimum_height_of_subtree(temp1->right);
+	else if (temp1->right == NULL)
+		return 1 + minimum_height_of_subtree(temp1->left);
+	else
+	{
+		if (1 + minimum_height_of_subtree(temp1->left) > 1 + minimum_height_of_subtree(temp1->right))
+			return 1 + minimum_height_of_subtree(temp1->right);
+		else
+			return 1 + minimum_height_of_subtree(temp1->left);
+	}
+}
+int closest_leaf_node(struct node *root, int key, int search_distance, int min)
+{
+	int  cur_distance;
+	struct node *temp;
+	for (temp = root; temp->data != key; search_distance--)
+	{
+		if (temp->data < key)
+		{
+			if (temp->left != NULL)
+			{
+				cur_distance = 1 + minimum_height_of_subtree(temp->left) + search_distance;
+				if (cur_distance < min)
+					min = cur_distance;
+			}
+			temp = temp->right;
+		}
+		else
+		{
+			if (temp->right != NULL)
+			{
+				cur_distance = 1 + minimum_height_of_subtree(temp->right) + search_distance;
+				if (cur_distance < min)
+					min = cur_distance;
+			}
+			temp = temp->left;
+		}
+		//	printf("\nmin\t%d\tcur_distance\t%d", min, cur_distance);
+	}
+	return min;
+}
 int get_closest_leaf_distance(struct node *root, struct node *temp)
 {
-  return -1;
+	int  search_distane = 0, min_height;
+	if (root == NULL || temp == NULL)
+		return -1;
+	struct node *key_node;
+	key_node = search(root, temp->data, &search_distane);
+	if (search_distane == -1)
+		return -1;
+	else
+	{
+		min_height = minimum_height_of_subtree(key_node);
+		return  closest_leaf_node(root, temp->data, search_distane, min_height);
+	}
 }
